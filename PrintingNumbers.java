da27a63ed695ba8a2d6cb8d24ead28047847e6c0
NAVITHA <167970442+navitha2408@users.noreply.github.com>
2025-06-17 14:42:32 +0530

    Add files via upload

---
+++ PrintingNumbers.java
@@ -0,0 +1,14 @@
+package xyz;
+
+public class PrintingNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=1;i<=10;i++) {
+			System.out.print(i+"\t");
+		}
+
+	}
+
+}
